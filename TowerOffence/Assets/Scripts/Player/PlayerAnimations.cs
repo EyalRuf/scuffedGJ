a99fc4c295c960c0353b77294d5277570873065d
Eyal Rufeisen <eyalrocks@gmail.com>
2020-11-13 13:05:30 +0100

    Towers, game end, respawns, playeranimations, animation controller, etc

---
+++ TowerOffence/Assets/Scripts/Player/PlayerAnimations.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+public class PlayerAnimations : MonoBehaviour
+{
+    const string ANIM_PROP_NAME_DIR = "direction";
+    const string ANIM_PROP_NAME_IS_WALKING = "isWalking";
+    const string ANIM_PROP_NAME_ATTACK = "attack";
+    const string ANIM_PROP_NAME_SLAP = "slap";
+
+    Animator pAnimator;
+    Player p;
+
+    // Use this for initialization
+    void Start()
+    {
+        pAnimator = GetComponent<Animator>();
+        p = GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pAnimator.SetInteger(ANIM_PROP_NAME_DIR, p.pMovement.direction);
+        pAnimator.SetBool(ANIM_PROP_NAME_IS_WALKING, p.pMovement.isWalking);
+    }
+
+    public void AttackAnimation()
+    {
+        pAnimator.SetTrigger(ANIM_PROP_NAME_ATTACK);
+    }
+
+    public void SlapAnimation()
+    {
+        pAnimator.SetTrigger(ANIM_PROP_NAME_SLAP);
+    }
+}
--- TowerOffence/Assets/Scripts/Player/PlayerLife.cs
+++ TowerOffence/Assets/Scripts/Player/PlayerLife.cs
@@ -5,18 +5,41 @@ public class PlayerLife : Destructable
 {
     public override void Destroy()
     {
-        Destroy(gameObject);
-    }
+        base.Destroy();
+        Player p = GetComponent<Player>();
 
-    // Use this for initialization
-    void Start()
-    {
+        if (p.pAttack.tool != null)
+        {
+            Destroy(p.pAttack.tool.gameObject);
+            p.pAttack.tool = null;
+        }
 
+        p.pAttack.enabled = false;
+        p.pInput.enabled = false;
+        p.pMovement.enabled = false;
+        p.pResources.enabled = false;
+        GetComponent<CircleCollider2D>().enabled = false;
+        GetComponent<SpriteRenderer>().enabled = false;
+
+        // MOVE PLAYER TO START POS?
     }
 
     // Update is called once per frame
-    void Update()
+    protected override void Update()
     {
+        base.Update();
+    }
 
+    protected override void Respawn()
+    {
+        base.Respawn();
+        Player p = GetComponent<Player>();
+        p.pAttack.enabled = true;
+        p.pInput.enabled = true;
+        p.pMovement.enabled = true;
+        p.pResources.enabled = true;
+        GetComponent<CircleCollider2D>().enabled = true;
+        GetComponent<SpriteRenderer>().enabled = true;
     }
 }
+
